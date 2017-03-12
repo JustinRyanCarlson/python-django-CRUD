8d7cf05c857e275eef8f139f288d92d2ae19ad15
Justin Carlson <justin.ryan.carlson@gmail.com>
2017-03-12 18:49:34 -0500

    adds in create route for the CRUD functionality, some templates, and a model for the snippets

--- crud/app/myapp/urls.py
+++ crud/app/myapp/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from . import views
 
+app_name = "snippets"
 urlpatterns = [
     # ^ means start of the line                   ie home2595438509284059820    works
     # $ means it should be the end of the line    ie /home/                     works
     url(r'^home/$', views.home, name='home'),
+    url(r'^create/$', views.create, name='create')
 ]
--- crud/app/myapp/views.py
+++ crud/app/myapp/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Snippet
+
 
 # Create your views here.
-
-
 def home(request):
-    return render(request, 'myapp/home.html.djt', {})
+    snippets = Snippet.objects.all()
+    context = {'snippets': snippets}
+    return render(request, 'myapp/home.html.djt', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        Snippet(
+            title=request.POST['title'],
+            description=request.POST['description'],
+            snippet=request.POST['snippet'],
+            lang=request.POST['lang']
+        ).save()
+
+    return render(request, 'myapp/create.html.djt', {})
